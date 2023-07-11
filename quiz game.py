010839d1789a585fb3c9aaa25e9897dc56f12e46
LeenaBhonde <139044441+LeenaBhonde@users.noreply.github.com>
2023-07-11 04:01:10 -0700

    Add files via upload

---
+++ quiz game.py
@@ -0,0 +1,115 @@
+print("Welcome to my computer quiz!")
+
+playing= input("Do you want to play? ")
+if playing != "yes":
+   quit()
+
+print("Okay! let's play:")
+point=0
+answer=input("Who is known as father of Internet?")
+if answer =="vinton cerf":
+   print("Correct!")
+   point=+1
+else:
+    print("Incorrect!")
+    print(point)
+
+
+answer1=input("Who invented Computer Mouse? ")
+if answer1 =="doughles engelbert":
+   print("Correct!")
+   point=+1
+else:
+    print("Incorrect!")
+     print(point)
+
+
+answer2=input("Who is known as the father of Free Software Foundation? ")
+if answer2 =="richard mathew stallman":
+   print("Correct!")
+   point=+1
+else:
+    print("Incorrect!")
+    print(point)
+
+
+
+answer3=input("Who is the first computer programmer? ")
+if answer3 =="ada lovelace":
+   print("Correct!")
+   point=+1
+else:
+    print("Incorrect!")
+    print(point)
+
+
+
+answer4=input("What is the name of World's first digital computer? ")
+if answer4 =="mark 1":
+   print("Correct!")
+   point=+1
+else:
+    print("Incorrect!")
+    print(point)
+
+answer5=input("Which was the first Pocket Computer? ")
+if answer5 =="SHARP PC1211":
+   print("Correct!")
+   point=+1
+else:
+    print("Incorrect!")
+    print(point)
+
+
+answer6=input("Speak to Tweet is a service given by which giant?")
+if answer6 =="google":
+   print("Correct!")
+   point=+1
+else:
+    print("Incorrect!")
+    print(point)
+
+
+
+answer7=input("'WIT' and NASDAW code of which Indian IT Company?")
+if answer7 =="wipro":
+   print("Correct!")
+   point=+1
+else:
+    print("Incorrect!")
+    print(point)
+
+
+answer7=input("Expand HDMI")
+if answer7 =="high definition multimedia interface":
+   print("Correct!")
+   point=+1
+else:
+    print("Incorrect!")
+    print(point)
+ 
+
+answer7=input("Who is known as the father of World Wide Web")
+if answer7 =="Tim Berners Lee":
+   print("Correct!")
+   point=+1
+else:
+    print("Incorrect!")
+    print(point)
+ 
+
+answer7=input("Which is the current version of Windoes running?")
+if answer7 =="windoes11":
+   print("Correct!")
+   point=+1
+else:
+    print("Incorrect!")
+    print(point)
+ 
+
+
+
+
+
+
+
